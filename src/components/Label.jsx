e0095c6ce166de78ba9c8415cbfa83fb6f797ade
RinaldoDwiFaturahman <faturahmanrinaldo@gmail.com>
2023-06-12 20:57:12 +0700

    feat: add design system

--- src/components/Label.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function Label({ className = 'mb-1 text-body-b font-normal', htmlFor, children, ...rest }) {
-    return (
-        <label {...rest} htmlFor={htmlFor} className={`${className} font-poppins`}>
-            {children}
-        </label>
-    );
-}
