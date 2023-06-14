58cb5bc17a773c10c4de1cd99fe18b7dad6d210e
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-06-14 07:58:50 +0700

    fix: fixing to original

--- src/app/page.js
+++ src/app/page.js
@@ -1,71 +1,12 @@
 'use client';
 
-import Login from '@/examples/Login';
-import CardExample from '@/examples/CardExample';
-import Image from 'next/image';
-import SearchCityModal from '@/examples/SearchCityModal';
-import ChoosePassengerTypeModal from '@/examples/ChoosePassengerTypeModal';
-import ChooseFlightClassModal from '@/examples/ChooseFlightClassModal';
-import ChooseFilterTicketModal from '@/examples/ChooseFilterTicketModal';
-import SearchBar from '@/components/SearchBar';
-import FlightCalendarPicker from '@/examples/FlightCalendarPicker';
-import CalendarPicker from '@/components/CalendarPicker';
-import ToggleRotate from '@/components/ToggleRotate';
-import HomeSearch from '@/components/HomeSearch';
-import Demo from '@/examples/Demo';
-import LoginResponsive from '@/examples/LoginResponsive';
-import RegisterResponsive from '@/examples/RegisterResponsive';
-import HomePageResponsive from '@/examples/HomePageResponsive';
-import Input from '@/components/Input';
-import SearchPage from '@/examples/SearchPage';
-import { FiSearch } from 'react-icons/fi';
-
-import { useState } from 'react';
-import Button from '@/components/Button';
-
-import ComponentDocs from '@/examples/ComponentDocs';
-
 export default function Home() {
     return (
-        <main>
-            {/* <Login /> */}
-            {/* <LoginResponsive /> */}
-            {/* <RegisterResponsive /> */}
-            {/* <HomePageResponsive /> */}
-            {/* <SearchPage /> */}
-            {/* <CardExample /> */}
-
-            {/* Buat Check Modal */}
-
-                {/* <CardExample /> */}
-                {/* <ChooseFilterTicketModal /> */}
-                {/* <ChooseFlightClassModal /> */}
-                {/* <ChoosePassengerTypeModal /> */}
-                <SearchCityModal />
-            {/* <SearchBar /> */}
-            {/* <FlightCalendarPicker /> */}
-            {/* <ToggleRotate /> */}
-            {/* <CalendarPicker open={true} /> */}
-            {/* <div className='flex items-center justify-center h-screen bg-slate-400'>
-                <div className='h-[292px] w-[968px]'>
-                    <HomeSearch />
-                </div>
-            </div> */}
-            {/* <div className='flex items-center justify-center h-screen bg-slate-300 '>
-                <div className='relative w-[500px]'>
-                    <Input className='rounded-rad-4 bg-net-6 py-[14px] pl-[24px] pr-[64px]' placeholder={'Cari di sini...'} />
-                    <FiSearch className='absolute right-0 top-1/2 mr-[17px] h-[20px] w-[20px] translate-y-[-50%] text-net-3' />
-                </div>
-            </div> */}
-
-            {/* <ComponentDocs /> */}
-        </main>
+        
+           <main className="flex flex-col items-center justify-between min-h-screen p-24">
+     <h1>This is main yaww :D</h1>
+    </main>
+       
     );
 }
-//items-center justify-center backdrop-blur-sm fixed inset-0 flex
-// Ngubah center dari atas ke bawah
-// atas: 50% || transform: translateY(-50%)
-// kanan: 50% || transform: translateX(-50%)
 
-// Layout
-// Component
