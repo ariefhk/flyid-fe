326a5b46adda91856c3041b87b620b3d14eb1c9a
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-06-30 18:35:08 +0700

    fix: dynamic date

--- src/utils/getDateInRange.js
+++ src/utils/getDateInRange.js
@@ -33,12 +33,15 @@
 // two way
 export const getDateInRange = (dateInput, returnInput = null) => {
     if (returnInput) {
-        const dates = [];
+        const testDate = new Date(dateInput);
+        const testDate2 = new Date(testDate.getTime());
+        const testDate3 = new Date(testDate2);
         const d1 = new Date(returnInput);
-        const d2 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate() - 7);
-        const d3 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate());
-        const date = new Date(d2.getTime());
+        d1.setHours(0, 0, 0, 0);
+        const d3 = new Date(d1.getFullYear(), d1.getMonth(), d1.getDate() + 1);
+        const date = new Date(testDate3.getTime());
         let angka = 1;
+        const dates = [];
 
         while (date <= d3) {
             dates.push({
